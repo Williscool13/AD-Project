2599afe4caeb0e1043ad83625e7d7c2926aa6c78
Williscool13 <50206340+Williscool13@users.noreply.github.com>
2020-01-06 17:56:08 +0800

    testing branch for database

---
+++ database_test.py
@@ -0,0 +1,34 @@
+import pymysql
+def main():
+    #### DO NOT TOUCH
+    host = 'ad-database.chexbadcv1tk.ap-southeast-1.rds.amazonaws.com'
+    database = 'adproject'
+    user = 'admin'
+    password = '1234567890'
+    port = 3306
+    connection = pymysql.connect(host=host, db=database, user=user, passwd=password, port=port)
+    cursor = connection.cursor()
+    #### DO NOT TOUCH###############################################################################
+    
+    command = 'select * from quiz'
+    cursor.execute(command)
+    print('Printing all table names: \n', cursor.fetchall())
+
+    #################################################################################################
+    print('program finished')
+
+if __name__ == '__main__':
+    main()
+
+
+
+#"""create table quiz(quizid int(50) not null auto_increment primary key, question varchar(100), options varchar(200), answer varchar(100))"""
+
+#command = "insert into quiz values(1,'1+1', '3|4|2|9', '2')"
+#cursor.execute(command)
+#connection.commit()
+
+#command = "insert into quiz values(2,'2+2', '6|4|8|11', '4')"
+#cursor.execute(command)
+#connection.commit()
+   
